9c413cfa6904a2e1042fe98dbab6c5777fbdeebe
Michael Glaser <m.glaser82@gmail.com>
2017-12-04 18:52:34 -0800

    Sets basic algorithm to pass old-roman-numerals tests. Converts 1 and 4.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,10 @@
 def convert_to_roman(arabic_number)
   # Your code goes here
+  number = arabic_number.to_i
+  roman_num_string = ""
+  while number > 0
+    roman_num_string += "I"
+    number -= 1
+  end
+  return roman_num_string
 end
