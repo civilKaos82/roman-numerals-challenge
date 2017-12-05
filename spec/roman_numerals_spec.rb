b56935a1931f0e9a735c3c625e9625be9da9757e
Michael Glaser <m.glaser82@gmail.com>
2017-12-04 19:21:49 -0800

    Creates rspec tests for the expected behavior for converting arabic numbers to roman numerals. Range is from 3000 down to 1.

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -13,6 +13,70 @@ describe 'converting an Arabic number to a Roman numeral' do
     # Release 1 ...
     # add tests for old roman numerals here
 
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 9 to VIIII' do
+      expect(convert_to_roman(9)).to eq "VIIII"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10)).to eq "X"
+    end
+
+    it 'converts 11 to XI' do
+      expect(convert_to_roman(11)).to eq "XI"
+    end
+
+    it 'converts 14 to XIIII' do
+      expect(convert_to_roman(14)).to eq "XIIII"
+    end
+
+    it 'converts 15 to XV' do
+      expect(convert_to_roman(15)).to eq "XV"
+    end
+
+    it 'converts 19 to XVIIII' do
+      expect(convert_to_roman(19)).to eq "XVIIII"
+    end
+
+    it 'converts 20 to XX' do
+      expect(convert_to_roman(20)).to eq "XX"
+    end
+
+    it 'converts 50 to L' do
+      expect(convert_to_roman(50)).to eq "L"
+    end
+
+    it 'converts 77 to LXXVII' do
+      expect(convert_to_roman(77)).to eq "LXXVII"
+    end
+
+    it 'converts 100 to C' do
+      expect(convert_to_roman(100)).to eq "C"
+    end
+
+    it 'converts 499 to CCCCLXXXXVIIII' do
+      expect(convert_to_roman(499)).to eq "CCCCLXXXXVIIII"
+    end
+
+    it 'converts 500 to D' do
+      expect(convert_to_roman(500)).to eq "D"
+    end
+
+    it 'converts 999 to DCCCCLXXXXVIIII' do
+      expect(convert_to_roman(999)).to eq "DCCCCLXXXXVIIII"
+    end
+
+    it 'converts 1000 to M' do
+      expect(convert_to_roman(1000)).to eq "M"
+    end
+
+    it 'converts 3000 to MMM' do
+      expect(convert_to_roman(3000)).to eq "MMM"
+    end
+
   end
 
   describe 'modern Roman numerals' do
