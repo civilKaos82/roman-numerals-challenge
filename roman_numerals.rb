347cfb03f4397ae37166dcd0ade3be666a5d60b2
Michael Glaser <m.glaser82@gmail.com>
2017-12-05 17:15:43 -0800

    Add ability to convert arabic numbers to old and new roman numerals. Default is set to old roman numerals.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,61 +1,39 @@
-def convert_to_roman(arabic_number)
+def convert_to_roman(arabic_number, modern = false)
   # Your code goes here
-  old_roman_hash = {
-    "M" => 1000,
-    "D" => 500,
-    "C" => 100,
-    "L" => 50,
-    "X" => 10,
-    "V" =>  5,
-    "I" =>  1
-  }
+  if modern == false
+    roman_hash = {
+      "M" => 1000,
+      "D" => 500,
+      "C" => 100,
+      "L" => 50,
+      "X" => 10,
+      "V" =>  5,
+      "I" =>  1
+    }
+  elsif modern == true
+    roman_hash = {
+      "M" => 1000,
+      "CM" => 900,
+      "D" => 500,
+      "CD" => 400,
+      "C" => 100,
+      "XC" => 90,
+      "L" => 50,
+      "XL" => 40,
+      "X" => 10,
+      "IX" => 9,
+      "V" =>  5,
+      "IV" => 4,
+      "I" =>  1
+    }
+  end
   numerator = arabic_number.to_i
   roman_num_string = ""
 
-  old_roman_hash.each do |numeral, number|
-
+  roman_hash.each do |numeral, number|
     roman_num_string += numeral * (numerator/number)
     numerator -= (number * (numerator/number))
   end
-
-  # while number > 0
-  #
-    # while number >= old_roman_hash["M"]
-    #   roman_num_string += "M"
-    #   number -= 1000
-    # end
-    #
-    # while number >= old_roman_hash["D"]
-    #   roman_num_string += "D"
-    #   number -= 500
-    # end
-    #
-    # while number >= old_roman_hash["C"]
-    #   roman_num_string += "C"
-    #   number -= 100
-    # end
-    #
-    # while number >= old_roman_hash["L"]
-    #   roman_num_string += "L"
-    #   number -= 50
-    # end
-    #
-    # while number >= old_roman_hash["X"]
-    #   roman_num_string += "X"
-    #   number -= 10
-    # end
-    #
-    # while number >= old_roman_hash["V"]
-    #   roman_num_string += "V"
-    #   number -= 5
-    # end
-    #
-    # while number >= old_roman_hash["I"]
-    #   roman_num_string += "I"
-    #   number -= 1
-    # end
-    #
-  # end
 
   return roman_num_string
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -82,6 +82,79 @@ describe 'converting an Arabic number to a Roman numeral' do
   describe 'modern Roman numerals' do
     # Release 3 ...
     # add tests for modern roman numerals here
+    it 'converts 1 to I' do
+      expect(convert_to_roman(1, modern = true)).to eq "I"
+    end
 
+    it 'converts 4 to IV' do
+      expect(convert_to_roman(4, modern = true)).to eq "IV"
+    end
+
+    # Release 1 ...
+    # add tests for old roman numerals here
+
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5, modern = true)).to eq "V"
+    end
+
+    it 'converts 9 to IX' do
+      expect(convert_to_roman(9, modern = true)).to eq "IX"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10, modern = true)).to eq "X"
+    end
+
+    it 'converts 11 to XI' do
+      expect(convert_to_roman(11, modern = true)).to eq "XI"
+    end
+
+    it 'converts 14 to XIV' do
+      expect(convert_to_roman(14, modern = true)).to eq "XIV"
+    end
+
+    it 'converts 15 to XV' do
+      expect(convert_to_roman(15, modern = true)).to eq "XV"
+    end
+
+    it 'converts 19 to XIX' do
+      expect(convert_to_roman(19, modern = true)).to eq "XIX"
+    end
+
+    it 'converts 20 to XX' do
+      expect(convert_to_roman(20, modern = true)).to eq "XX"
+    end
+
+    it 'converts 50 to L' do
+      expect(convert_to_roman(50, modern = true)).to eq "L"
+    end
+
+    it 'converts 77 to LXXVII' do
+      expect(convert_to_roman(77, modern = true)).to eq "LXXVII"
+    end
+
+    it 'converts 100 to C' do
+      expect(convert_to_roman(100, modern = true)).to eq "C"
+    end
+
+    it 'converts 499 to CDXCIX' do
+      expect(convert_to_roman(499, modern = true)).to eq "CDXCIX"
+    end
+
+    it 'converts 500 to D' do
+      expect(convert_to_roman(500, modern = true)).to eq "D"
+    end
+
+    it 'converts 999 to CMXCIX' do
+      expect(convert_to_roman(999, modern = true)).to eq "CMXCIX"
+    end
+
+    it 'converts 1000 to M' do
+      expect(convert_to_roman(1000, modern = true)).to eq "M"
+    end
+
+    it 'converts 3000 to MMM' do
+      expect(convert_to_roman(3944, modern = true)).to eq "MMMCMXLIV"
+    end
   end
 end
