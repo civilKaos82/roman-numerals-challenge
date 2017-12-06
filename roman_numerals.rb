74dc715b4dd5d9f4b760680f25fcd7bf20e9a60b
Michael Glaser <m.glaser82@gmail.com>
2017-12-05 16:39:27 -0800

    Refactors code for old roman numerals algorythm. Code is more DRY and condensed. Tests still pass.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,5 @@
 def convert_to_roman(arabic_number)
   # Your code goes here
-  number = arabic_number.to_i
-  roman_num_string = ""
   old_roman_hash = {
     "M" => 1000,
     "D" => 500,
@@ -11,45 +9,53 @@ def convert_to_roman(arabic_number)
     "V" =>  5,
     "I" =>  1
   }
+  numerator = arabic_number.to_i
+  roman_num_string = ""
 
-  while number > 0
+  old_roman_hash.each do |numeral, number|
 
-    while number >= old_roman_hash["M"]
-      roman_num_string += "M"
-      number -= 1000
-    end
-
-    while number >= old_roman_hash["D"]
-      roman_num_string += "D"
-      number -= 500
-    end
-
-    while number >= old_roman_hash["C"]
-      roman_num_string += "C"
-      number -= 100
-    end
-
-    while number >= old_roman_hash["L"]
-      roman_num_string += "L"
-      number -= 50
-    end
-
-    while number >= old_roman_hash["X"]
-      roman_num_string += "X"
-      number -= 10
-    end
-
-    while number >= old_roman_hash["V"]
-      roman_num_string += "V"
-      number -= 5
-    end
-
-    while number >= old_roman_hash["I"]
-      roman_num_string += "I"
-      number -= 1
-    end
-
+    roman_num_string += numeral * (numerator/number)
+    numerator -= (number * (numerator/number))
   end
+
+  # while number > 0
+  #
+    # while number >= old_roman_hash["M"]
+    #   roman_num_string += "M"
+    #   number -= 1000
+    # end
+    #
+    # while number >= old_roman_hash["D"]
+    #   roman_num_string += "D"
+    #   number -= 500
+    # end
+    #
+    # while number >= old_roman_hash["C"]
+    #   roman_num_string += "C"
+    #   number -= 100
+    # end
+    #
+    # while number >= old_roman_hash["L"]
+    #   roman_num_string += "L"
+    #   number -= 50
+    # end
+    #
+    # while number >= old_roman_hash["X"]
+    #   roman_num_string += "X"
+    #   number -= 10
+    # end
+    #
+    # while number >= old_roman_hash["V"]
+    #   roman_num_string += "V"
+    #   number -= 5
+    # end
+    #
+    # while number >= old_roman_hash["I"]
+    #   roman_num_string += "I"
+    #   number -= 1
+    # end
+    #
+  # end
 
   return roman_num_string
 end
